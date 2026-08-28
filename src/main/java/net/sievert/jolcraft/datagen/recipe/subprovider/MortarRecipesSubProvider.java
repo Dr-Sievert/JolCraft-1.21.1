a075b790fdcdc7c6382c9408ba15cefb78699bbd
Dr-Sievert <thsi.privat@gmail.com>
2026-08-28 12:17:22 +0200

    mortar vanilla recipes

--- src/main/java/net/sievert/jolcraft/datagen/recipe/subprovider/MortarRecipesSubProvider.java
+++ src/main/java/net/sievert/jolcraft/datagen/recipe/subprovider/MortarRecipesSubProvider.java
@@ -118,5 +118,337 @@ public record MortarRecipesSubProvider(
                         .toolDamage(1)
                         .buildValidated()
         );
+
+        //Vanilla extras
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.BONE)
+                        .result(Items.BONE_MEAL, 5)
+                        .grindingWork(200)
+                        .toolDamage(2)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.BREEZE_ROD)
+                        .result(Items.WIND_CHARGE, 8)
+                        .grindingWork(200)
+                        .toolDamage(2)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.BLAZE_ROD)
+                        .result(Items.BLAZE_POWDER, 3)
+                        .grindingWork(300)
+                        .toolDamage(3)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.LILY_OF_THE_VALLEY)
+                        .result(Items.WHITE_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.BONE_MEAL)
+                        .result(Items.WHITE_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.BONE_MEAL)
+                        .result(Items.WHITE_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.COCOA_BEANS)
+                        .result(Items.BROWN_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.ORANGE_TULIP)
+                        .result(Items.ORANGE_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.TORCHFLOWER)
+                        .result(Items.ORANGE_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.SEA_PICKLE)
+                        .result(Items.LIME_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.PITCHER_PLANT)
+                        .result(Items.CYAN_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.LAPIS_LAZULI)
+                        .result(Items.BLUE_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.CORNFLOWER)
+                        .result(Items.BLUE_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.ALLIUM)
+                        .result(Items.MAGENTA_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.LILAC)
+                        .result(Items.MAGENTA_DYE, 4)
+                        .grindingWork(20)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.AZURE_BLUET)
+                        .result(Items.LIGHT_GRAY_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.OXEYE_DAISY)
+                        .result(Items.LIGHT_GRAY_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.WHITE_TULIP)
+                        .result(Items.LIGHT_GRAY_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.INK_SAC)
+                        .result(Items.BLACK_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.WITHER_ROSE)
+                        .result(Items.BLACK_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.POPPY)
+                        .result(Items.RED_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.RED_TULIP)
+                        .result(Items.RED_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.BEETROOT)
+                        .result(Items.RED_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.ROSE_BUSH)
+                        .result(Items.RED_DYE, 4)
+                        .grindingWork(20)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.DANDELION)
+                        .result(Items.YELLOW_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.SUNFLOWER)
+                        .result(Items.YELLOW_DYE, 4)
+                        .grindingWork(20)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.CACTUS)
+                        .result(Items.GREEN_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.BLUE_ORCHID)
+                        .result(Items.LIGHT_BLUE_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.PINK_TULIP)
+                        .result(Items.PINK_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
+
+        emit(
+                output,
+                tracking,
+                MortarRecipeBuilder.create()
+                        .input(Items.PINK_PETALS)
+                        .result(Items.PINK_DYE, 2)
+                        .grindingWork(10)
+                        .toolDamage(1)
+                        .buildValidated()
+        );
     }
 }
